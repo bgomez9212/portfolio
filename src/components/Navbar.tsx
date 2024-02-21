62d1f9f5d30203b0846dc89256953cb4a6dac377
Brandon Gomez <bgomez9212@gmail.com>
2024-02-21 11:46:40 -0800

    add bg and shadow to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,31 @@
 import { useState, useEffect } from "react";
 
 export default function Navbar() {
-  const [dynamicHeight, setDynamicHeight] = useState({ pic: 200, pad: 20 });
+  const [dynamicHeight, setDynamicHeight] = useState({
+    pic: 200,
+    pad: 20,
+    shadow: 0,
+  });
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const newPicHeight = Math.max(200 - scrollPosition * 0.2, 75);
       const newPadding = Math.max(20 - scrollPosition * 0.02, 6);
-      setDynamicHeight({ pic: newPicHeight, pad: newPadding });
+      const newShadow = Math.min(scrollPosition * 0.25, 10);
+      console.log(newShadow);
+      setDynamicHeight({
+        pic: newPicHeight,
+        pad: newPadding,
+        shadow: newShadow,
+      });
     };
 
     window.addEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className="flex-col flex items-center fixed top-0 left-0 right-0">
+    <div className="flex-col flex items-center fixed top-0 left-0 right-0 bg-white">
       <div className="w-full flex justify-center mb-5">
         <div
           className="flex items-center justify-center border-b-2 w-[90%]"
@@ -38,11 +48,15 @@ export default function Navbar() {
           <h1 className="text-2xl">Software Engineer</h1>
         </div>
       </div>
-      <div className="w-1/2 flex justify-between">
-        <a>Personal Projects</a>
-        <a>HackReactor</a>
-        <a>Scrimba</a>
-        <a>Contact</a>
+      <div
+        className={`w-[90%] flex justify-center shadow-[rgba(0,0,10,0.2)_0px_${dynamicHeight.shadow}px_0px_0px] pb-3`}
+      >
+        <div className="w-1/2 flex justify-between">
+          <a>Personal Projects</a>
+          <a>HackReactor</a>
+          <a>Scrimba</a>
+          <a>Contact</a>
+        </div>
       </div>
     </div>
   );
