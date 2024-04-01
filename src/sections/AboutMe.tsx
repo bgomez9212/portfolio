1f7ce50b38a2ab37abc7cac04d778011409b6022
Brandon Gomez <bgomez9212@gmail.com>
2024-03-31 20:47:40 -0700

    change about me description

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,24 +1,26 @@
 export default function AboutMe() {
   return (
     <div className="w-3/4 sm:w-3/4 text-center text-md flex pt-96">
-      Hello and welcome to my portfolio site. You are welcome to either scroll
-      down through the sections, or click through the navigation to jump you to
-      the section you would like to see. A little bit about myself: I've worked
-      in manual labor for about decade and spent some time doing inventory
-      control. Neither of these were particularly exciting so I decided to make
-      a total career change and jump into the land of software engineering. I'm
-      finding building applications and websites, as well as working with like
-      minded individuals to be a lot more rewarding. And there is always an
-      abundance of information to learn in this field that I don't see myself
-      becoming complacent in this line of work. My journey started with Scrimba
-      which is a self-paced front end course and I think it was a really good
-      introduction to software engineering. I then enrolled in HackReactor and
-      completed the 12 week immersive where I worked with my peers to complete
-      sprints and this was great practice for working in teams and planning
-      projects from start to finish, as well as working against a deadline.
-      After graduating from HackReactor I've found myself trying to learn more
-      to become a better programmer and ideally a valuable asset to a company.
-      If you've made it this far, thanks for reading!
+      Hello, I am Brandon Gomez, a software engineer currently located in Reno,
+      Nevada. Equipped with a solid foundation of both back-end and front-end
+      technologies, I specialize in building applications from inception to
+      fruition, while continuously working to master new technologies and design
+      principles. Possessing skills of collaboration, communication, and
+      organization from a decade in labor, I have taken these abilities and
+      applied them to a newfound career - software development. Starting with
+      Scrimba, a program aimed to teach the fundamentals of front-end
+      development, to Hack Reactor's immersive course, I have either created or
+      made significant contributions to a number of projects. Utilizing
+      templates provided by clients, or designing a website based off of
+      business requirements, I have demonstrated versatility in working
+      independently as well as collaboratively within teams of my peers. Using
+      technologies such as React, Javascript, TailwindCSS, NodeJS, Express,
+      Firebase, PostgreSQL, MongoDB, AWS and Figma, I have created websites from
+      back to front, designed intuitive user interfaces, created APIs, optimized
+      servers, and deployed applications. I invite you to explore my projects
+      and experience firsthand the quality that defines my work and learning
+      experience. Whether you're looking to collaborate on a project or explore
+      job opportunities, I'm open to connecting with you.
     </div>
   );
 }
