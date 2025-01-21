3f774f2801b607b3edfffc484fcec2d2c57a1e45
Brandon Gomez <bgomez9212@gmail.com>
2025-01-20 18:45:40 -0800

    changed about me

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,26 +1,9 @@
 export default function AboutMe() {
   return (
-    <div className="w-3/4 sm:w-3/4 text-center text-md flex pt-96 z-0">
-      Hello, I am Brandon Gomez, a software engineer currently located in Reno,
-      Nevada. Equipped with a solid foundation of both back-end and front-end
-      technologies, I specialize in building applications from inception to
-      fruition, while continuously working to master new technologies and design
-      principles. Possessing skills of collaboration, communication, and
-      organization from a decade in labor, I have taken these abilities and
-      applied them to a newfound career - software development. Starting with
-      Scrimba, a program aimed to teach the fundamentals of front-end
-      development, to Hack Reactor's immersive course, I have either created or
-      made significant contributions to a number of projects. Utilizing
-      templates provided by clients, or designing a website based off of
-      business requirements, I have demonstrated versatility in working
-      independently as well as collaboratively within teams of my peers. Using
-      technologies such as React, Javascript, TailwindCSS, NodeJS, Express,
-      Firebase, PostgreSQL, MongoDB, AWS and Figma, I have created websites from
-      back to front, designed intuitive user interfaces, created APIs, optimized
-      servers, and deployed applications. I invite you to explore my projects
-      and experience firsthand the quality that defines my work and learning
-      experience. Whether you're looking to collaborate on a project or explore
-      job opportunities, I'm open to connecting with you.
+    <div className="w-3/4 sm:w-3/4 text-center text-md pt-96 z-0">
+      Welcome! I am a fullstack web and mobile application developer based in
+      Reno, NV, that specializes in the PERN stack. <br />I have brought some of
+      my own ideas to life, and now I'm eager to help you do the same.
     </div>
   );
 }
