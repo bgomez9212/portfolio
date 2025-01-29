660e87d968789c873804fc90c3eefcc41b282dbc
Brandon Gomez <bgomez9212@gmail.com>
2025-01-28 21:02:03 -0800

    macbook mockup

--- src/components/MacbookMockup.tsx
+++ src/components/MacbookMockup.tsx
@@ -1,11 +1,13 @@
 import macbook from "../../public/macbook.png";
 
-export default function Macbook({ screenshot }: { screenshot: string }) {
+export default function MacbookMockup({ image }: { image: string }) {
   return (
-    <div className="relative">
-      <img className="block" src={macbook} />
-      <div className="absolute top-[5%] bottom-[12%] right-[11.5%] left-[11.5%] overflow-hidden hover:shadow-custom hover:cursor-pointer">
-        <img src={screenshot} className="brightness-75 hover:brightness-100" />
+    <div className="h-full">
+      <div className="relative">
+        <img className="block" src={macbook} />
+        <div className="absolute top-[5%] bottom-[12%] right-[11.5%] left-[11.5%] overflow-hidden hover:shadow-custom hover:cursor-pointer">
+          <img src={image} className="brightness-75 hover:brightness-100" />
+        </div>
       </div>
     </div>
   );
