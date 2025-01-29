3ebfe8fdb877171b030c31aa7f26a4487888529d
Brandon Gomez <bgomez9212@gmail.com>
2025-01-28 21:02:16 -0800

    card reformatting

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import HackReactor from "./sections/HackReactor";
 import PersonalProjects from "./sections/PersonalProjects";
 import Scrimba from "./sections/Scrimba";
 import { ThemeProvider } from "./components/ThemeProvider";
+import ProjectCard from "./components/ProjectCard";
+import mealmachine from "../public/mealmachine.png";
 
 function App() {
   return (
     <ThemeProvider>
       <div className="flex flex-col items-center dark:bg-deepSeaBlue dark:text-white">
-        <Navbar />
+        <ProjectCard cardType="Macbook" image={mealmachine} />
+        {/* <Navbar />
         <AboutMe />
         <PersonalProjects />
         <HackReactor />
         <Scrimba />
-        <Contact />
+        <Contact /> */}
       </div>
     </ThemeProvider>
   );
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,6 @@
+import IPhoneMockup from "./IPhoneMockup";
+import MacbookMockup from "./MacbookMockup";
+
 type ProjectCardProps = {
   projectName: string;
   description: string;
@@ -7,52 +10,76 @@ type ProjectCardProps = {
   demoLink?: string;
 };
 
+// export default function ProjectCard({
+//   projectName,
+//   description,
+//   deploymentLink,
+//   githubLink,
+//   figmaLink,
+//   demoLink,
+// }: ProjectCardProps) {
+//   return (
+//     <>
+//       <div className="flex-col block border border-gray mb-5 p-5 w-full scroll-view z-0 rounded-xl">
+//         <h1 className="text-3xl mb-3">{projectName}</h1>
+//         <p className="mb-3">{description}</p>
+//         <div className="flex justify-end">
+//           <div className="flex justify-between">
+//             {deploymentLink ? (
+//               <a
+//                 className="hover:underline underline-offset-8"
+//                 href={deploymentLink}
+//               >
+//                 Deployment Link
+//               </a>
+//             ) : demoLink ? (
+//               <a className="hover:underline underline-offset-8" href={demoLink}>
+//                 Demo Link
+//               </a>
+//             ) : null}
+//             {githubLink && (
+//               <a
+//                 className="hover:underline underline-offset-8 ml-5"
+//                 href={githubLink}
+//               >
+//                 GitHub Repo
+//               </a>
+//             )}
+//             {figmaLink && (
+//               <a
+//                 className="hover:underline underline-offset-8 ml-5"
+//                 href={figmaLink}
+//               >
+//                 Figma Design
+//               </a>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
 export default function ProjectCard({
-  projectName,
-  description,
-  deploymentLink,
-  githubLink,
-  figmaLink,
-  demoLink,
-}: ProjectCardProps) {
+  cardType,
+  image,
+}: {
+  cardType: "IPhone" | "Macbook";
+  image: string;
+}) {
   return (
-    <>
-      <div className="flex-col block border border-gray mb-5 p-5 w-full scroll-view z-0 rounded-xl">
-        <h1 className="text-3xl mb-3">{projectName}</h1>
-        <p className="mb-3">{description}</p>
-        <div className="flex justify-end">
-          <div className="flex justify-between">
-            {deploymentLink ? (
-              <a
-                className="hover:underline underline-offset-8"
-                href={deploymentLink}
-              >
-                Deployment Link
-              </a>
-            ) : demoLink ? (
-              <a className="hover:underline underline-offset-8" href={demoLink}>
-                Demo Link
-              </a>
-            ) : null}
-            {githubLink && (
-              <a
-                className="hover:underline underline-offset-8 ml-5"
-                href={githubLink}
-              >
-                GitHub Repo
-              </a>
-            )}
-            {figmaLink && (
-              <a
-                className="hover:underline underline-offset-8 ml-5"
-                href={figmaLink}
-              >
-                Figma Design
-              </a>
-            )}
-          </div>
-        </div>
+    <div className="border border-redAlert bg-gradient-to-r from-shadowBlue to-transparent to-75% rounded-lg w-4/5 flex my-10">
+      <div className="">
+        <h1 className="text-4xl mb-2">Company/App</h1>
+        <h3 className="text-sm italic mb-10">Job Title</h3>
+        <p className="mb-2">Summary</p>
+        <div>Technologies</div>
       </div>
-    </>
+      {/* {cardType === "IPhone" ? (
+        <IPhoneMockup image={image} />
+      ) : (
+        <MacbookMockup image={image} />
+      )} */}
+    </div>
   );
 }
