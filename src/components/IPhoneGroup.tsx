b1d462571a84a666ffa9422a495d6a5d8e74bc14
Brandon Gomez <bgomez9212@gmail.com>
2025-02-05 20:30:04 -0800

    iphone group formatting

--- src/components/IPhoneGroup.tsx
+++ src/components/IPhoneGroup.tsx
@@ -1,16 +1,24 @@
 import IPhoneMockup from "./IPhoneMockup";
 
-export default function IPhoneGroup({ image }: { image: string }) {
+export default function IPhoneGroup({
+  image1,
+  image2,
+  image3,
+}: {
+  image1: string;
+  image2: string;
+  image3: string;
+}) {
   return (
     <div className="flex relative justify-center items-center w-full mx-[8%]">
       <div className="translate-x-10 -rotate-12">
-        <IPhoneMockup image={image} />
+        <IPhoneMockup image={image1} />
       </div>
       <div className="-translate-y-3">
-        <IPhoneMockup image={image} />
+        <IPhoneMockup image={image2} />
       </div>
       <div className="-translate-x-10 rotate-12">
-        <IPhoneMockup image={image} />
+        <IPhoneMockup image={image3} />
       </div>
     </div>
   );
