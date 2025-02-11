e9e999ffbc610cfe4531d0359de5c8e7f2ae3c60
Brandon Gomez <bgomez9212@gmail.com>
2025-02-10 18:14:06 -0800

    technology attributes object created

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,22 @@
 import IPhoneGroup from "./IPhoneGroup";
 import MacbookMockup from "./MacbookMockup";
 
+const techArr = [
+  { name: "expo", image: "/expo.png", link: "https://docs.expo.dev/" },
+  { name: "react", image: "/react.png", link: "https://react.dev/" },
+  {
+    name: "typescript",
+    image: "/typescript.png",
+    link: "https://www.typescriptlang.org/docs/",
+  },
+  {
+    name: "tailwind",
+    image: "/tailwind.png",
+    link: "https://v2.tailwindcss.com/docs",
+  },
+  { name: "vite", image: "/vite.png", link: "https://vite.dev/" },
+];
+
 export default function ProjectCard({
   cardType,
   image1,
@@ -18,7 +34,7 @@ export default function ProjectCard({
   project: string;
   jobTitle: string;
   summary: string;
-  technologies: string;
+  technologies: string[];
 }) {
   image2 = image2 || "/placeholder.png";
   image3 = image3 || "/placeholder.png";
@@ -27,13 +43,26 @@ export default function ProjectCard({
       id={`${cardType === "IPhone" ? "border-phone" : "border-macbook"}`}
       className={`rounded-lg w-4/5 flex my-10 justify-between px-10 max-h-[275px] relative`}
     >
-      <div id="text" className="flex flex-col justify-between py-10 w-1/2">
+      <div id="text" className="flex flex-col py-10 justify-between">
         <div>
           <h1 className="text-4xl mb-2">{project}</h1>
           <h3 className="text-sm italic">{jobTitle}</h3>
         </div>
-        <p>{summary}</p>
-        <div>{technologies}</div>
+        <p className="">{summary}</p>
+        <div className="flex items-center h-6">
+          {techArr.map((tech) => {
+            if (technologies.indexOf(tech.name) > -1) {
+              return (
+                <a href={tech.link} target="_blank">
+                  <img
+                    className="h-5 m-1 cursor-pointer transform transition duration-300 ease-in-out hover:scale-150"
+                    src={tech.image}
+                  />
+                </a>
+              );
+            }
+          })}
+        </div>
       </div>
       <div className="flex items-center justify-center w-1/2">
         {cardType === "Macbook" ? (
