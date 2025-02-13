b151722b8583834001bc814db40f24aa9efe32f9
Brandon Gomez <bgomez9212@gmail.com>
2025-02-12 18:46:13 -0800

    formatting changes for image within mockup

--- src/components/MacbookMockup.tsx
+++ src/components/MacbookMockup.tsx
@@ -4,8 +4,11 @@ export default function MacbookMockup({ image }: { image: string }) {
   return (
     <div className="relative max-w-[550px]">
       <img src={macbook} />
-      <div className="absolute top-[5%] bottom-[12%] right-[11.5%] left-[11.5%] hover:shadow-custom hover:cursor-pointer">
-        <img src={image} className="brightness-75 hover:brightness-100" />
+      <div className="absolute top-[5%] bottom-[12%] right-[11.5%] left-[11.5%] hover:shadow-custom hover:cursor-pointer overflow-hidden">
+        <img
+          src={image}
+          className="brightness-75 hover:brightness-100 object-fill w-full h-full"
+        />
       </div>
     </div>
   );
