a8240f9ffb1c27238e75f9828f26be51833ffee1
Brandon Gomez <bgomez9212@gmail.com>
2025-03-04 19:28:42 -0800

    add floating arrow

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function AboutMe() {
   return (
-    <div className="w-3/4 sm:w-3/4 flex text-center text-md h-screen z-0 justify-center items-center">
-      Welcome! I am a fullstack web and mobile application developer based in
-      Reno, NV, that specializes in the PERN stack. <br />I have brought some of
-      my own ideas to life, and now I'm eager to help you do the same.
+    <div className="w-3/4 sm:w-3/4 flex flex-col text-center text-md h-screen z-0 justify-between items-center pt-[400px]">
+      <div>
+        Welcome! I am a fullstack web and mobile application developer based in
+        Reno, NV, that specializes in the PERN stack. <br />I have brought some
+        of my own ideas to life, and now I'm eager to help you do the same.
+      </div>
+      <div>
+        <img className="moveArrow opacity-50" src="/down.png" />
+      </div>
     </div>
   );
 }
