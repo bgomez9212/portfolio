0fa123eb5305a719a4bd5bcac8e41ecee1979608
Brandon Gomez <bgomez9212@gmail.com>
2025-03-04 20:03:24 -0800

    hide arrows on scroll

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 
 export default function AboutMe() {
+  const [position, setPosition] = useState(0);
+  useEffect(() => {
+    function handleScroll() {
+      setPosition(window.scrollY);
+    }
+    window.addEventListener("scroll", handleScroll);
+  }, [position]);
+
   return (
     <div className="w-3/4 sm:w-3/4 flex flex-col text-center text-md h-screen z-0 justify-between items-center pt-[400px]">
       <div>
@@ -8,9 +16,11 @@ export default function AboutMe() {
         Reno, NV, that specializes in the PERN stack. <br />I have brought some
         of my own ideas to life, and now I'm eager to help you do the same.
       </div>
-      <div>
-        <img className="moveArrow opacity-50" src="/down.png" />
-      </div>
+      {position === 0 && (
+        <div>
+          <img className="moveArrow opacity-50" src="/down.png" />
+        </div>
+      )}
     </div>
   );
 }
